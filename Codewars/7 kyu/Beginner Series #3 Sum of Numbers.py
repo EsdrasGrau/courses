967b3c6a0184f7aca0098884a13e5a00f967fb9f
EsdrasGrau <esdrascampos@mac.com>
2020-06-19 15:51:02 -0500

    uploading files

--- Codewars/7 kyu/Beginner Series #3 Sum of Numbers.py
+++ Codewars/7 kyu/Beginner Series #3 Sum of Numbers.py
@@ -1,9 +1,15 @@
-def get_sum(a,b):
+import pandas as pd
+
+
+def get_sum(a, b):
     sum = 0
     if a == b:
         return a
     else:
         for i in range(a, b + 1):
-          sum += i
+            sum += i
 
     return sum
+
+
+df = df.to_read
--- Codewars/7 kyu/Sum of two lowest positive integers.py
+++ Codewars/7 kyu/Sum of two lowest positive integers.py
@@ -1,4 +1,10 @@
+import pandas as pd
+
+
 def sum_two_smallest_numbers(numbers):
     numbers.sort()
     res = numbers[0] + numbers[1]
     return res
+
+
+print('Is working')
