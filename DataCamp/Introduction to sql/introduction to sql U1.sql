e26233fb49c04223e4c8d44cc818c01dd5c33261
EsdrasGrau <esdrascampos@mac.com>
2020-06-22 16:32:48 -0500

    uploading quearies

--- DataCamp/Introduction to sql/introduction to sql U1.sql
+++ DataCamp/Introduction to sql/introduction to sql U1.sql
@@ -81,3 +81,54 @@ FROM films
 WHERE (release_year >= 1990 AND release_year < 2000)
 AND (language = 'French' OR language = 'Spanish')
 AND (gross > 2000000);
+
+
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000
+AND (language = 'Spanish' OR language = 'French')
+AND budget >100000000;
+
+SELECT title, release_year
+FROM films
+WHERE release_year IN (1990,2000)
+AND duration > 120;
+
+SELECT title, language
+FROM films
+WHERE language IN ('English', 'Spanish', 'French');
+
+SELECT title, certification
+FROM films
+WHERE certification IN ('NC-17','R');
+
+SELECT name
+FROM people
+WHERE deathdate IS NULL;
+
+SELECT title
+FROM films
+WHERE budget IS NULL;
+
+SELECT COUNT(title)
+FROM films
+WHERE language IS NULL;
+
+SELECT name
+FROM people
+WHERE name LIKE 'B%';
+
+SELECT name
+FROM people
+WHERE name LIKE '_r%';
+
+SELECT name
+FROM people
+WHERE name NOT LIKE 'A%';
+
+# Chapter 3
+SELECT AVG(duration)
+FROM films;
+
+SELECT MAX(duration)
+FROM films;
