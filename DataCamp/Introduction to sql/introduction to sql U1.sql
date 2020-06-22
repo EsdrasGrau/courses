20f13ca43b0a94c91be95504a4b4a39caed7a8e7
EsdrasGrau <esdrascampos@mac.com>
2020-06-22 15:02:28 -0500

    uploading queries

--- DataCamp/Introduction to sql/introduction to sql U1.sql
+++ DataCamp/Introduction to sql/introduction to sql U1.sql
@@ -34,5 +34,50 @@ FROM films;
 SELECT COUNT (DISTINCT country)
 FROM films;
 
-
 # Chapter 2
+SELECT *
+FROM films
+WHERE release_year = 2016;
+
+SELECT COUNT(title)
+FROM films
+WHERE release_year < 2000;
+
+SELECT title, release_year
+FROM films
+WHERE release_year > 2000;
+
+SELECT *
+FROM films
+WHERE language = 'French';
+
+SELECT name, birthdate
+FROM people
+WHERE birthdate = '1974-11-11'
+
+SELECT COUNT(title)
+FROM films
+WHERE language = 'Hindi';
+
+SELECT *
+FROM films
+WHERE certification = 'R';
+
+SELECT title, release_year
+FROM films
+WHERE language = 'Spanish' AND release_year < 2000;
+
+SELECT *
+FROM films
+WHERE language = 'Spanish' AND release_year > 2000;
+
+SELECT *
+FROM films
+WHERE language = 'Spanish' AND release_year > 2000
+AND release_year < 2010;
+
+SELECT title, release_year
+FROM films
+WHERE (release_year >= 1990 AND release_year < 2000)
+AND (language = 'French' OR language = 'Spanish')
+AND (gross > 2000000);
