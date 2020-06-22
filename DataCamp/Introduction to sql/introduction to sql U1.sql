658697d36cb87dc8dbebf68d1bdd142c1eb29a71
EsdrasGrau <esdrascampos@mac.com>
2020-06-22 13:12:52 -0500

    uploading files

---
+++ DataCamp/Introduction to sql/introduction to sql U1.sql
@@ -0,0 +1,38 @@
+# Chapter 1
+SELECT name FROM people;
+
+SELECT title, release_year, country
+FROM films;
+
+SELECT *
+FROM films;
+
+SELECT DISTINCT country
+FROM films;
+
+SELECT DISTINCT certification
+FROM films;
+
+SELECT DISTINCT role
+FROM roles;
+
+SELECT COUNT (*)
+FROM reviews;
+
+SELECT COUNT(*)
+FROM people;
+
+SELECT COUNT(birthdate)
+FROM people;
+
+SELECT COUNT(DISTINCT birthdate)
+FROM people;
+
+SELECT COUNT (DISTINCT language)
+FROM films;
+
+SELECT COUNT (DISTINCT country)
+FROM films;
+
+
+# Chapter 2
