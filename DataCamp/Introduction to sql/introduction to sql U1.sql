10818305c458bf7d83e6565261a703c255a64473
EsdrasGrau <esdrascampos@mac.com>
2020-06-23 14:09:04 -0500

    uploading files

--- DataCamp/Introduction to sql/introduction to sql U1.sql
+++ DataCamp/Introduction to sql/introduction to sql U1.sql
@@ -1,4 +1,4 @@
-# Chapter 1
+-- Chapter 1
 SELECT name FROM people;
 
 SELECT title, release_year, country
@@ -34,7 +34,7 @@ FROM films;
 SELECT COUNT (DISTINCT country)
 FROM films;
 
-# Chapter 2
+-- Chapter 2
 SELECT *
 FROM films
 WHERE release_year = 2016;
@@ -126,9 +126,48 @@ SELECT name
 FROM people
 WHERE name NOT LIKE 'A%';
 
-# Chapter 3
+-- Chapter 3
 SELECT AVG(duration)
 FROM films;
 
 SELECT MAX(duration)
 FROM films;
+
+SELECT SUM(gross)
+FROM films;
+
+SELECT SUM(gross)
+FROM films
+WHERE release_year >= 2000;
+
+SELECT AVG(gross)
+FROM films
+WHERE title like 'A%';
+
+SELECT MIN(gross)
+FROM films
+WHERE release_year = 1994;
+
+SELECT MAX(gross)
+FROM films
+WHERE release_year BETWEEN 2000 AND 2012;
+
+SELECT title, gross - budget AS net_profit
+FROM films;
+
+SELECT title, duration / 60.0 AS duration_hours
+FROM films;
+
+SELECT AVG(duration)/60.0 AS avg_duration_hours
+FROM films;
+
+SELECT (COUNT(deathdate) * 100.0) / COUNT(*) AS percentage_dead
+FROM people;
+
+SELECT MAX(release_year) - MIN(release_year) AS difference
+FROM films;
+
+SELECT (MAX(release_year) - MIN(release_year)) / 10 AS number_of_decades
+FROM films;
+
+-- Chapter 4
