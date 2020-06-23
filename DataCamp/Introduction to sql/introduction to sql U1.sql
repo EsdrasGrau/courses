3062a652bd62d3a32422d03d2f58f2d9bb9240c1
EsdrasGrau <esdrascampos@mac.com>
2020-06-23 17:07:43 -0500

    uploading all queries from U1

--- DataCamp/Introduction to sql/introduction to sql U1.sql
+++ DataCamp/Introduction to sql/introduction to sql U1.sql
@@ -237,3 +237,54 @@ GROUP BY release_year;
 SELECT release_year, MAX(budget)
 FROM films
 GROUP BY release_year;
+
+SELECT imdb_score, COUNT(*)
+FROM reviews
+GROUP BY imdb_score;
+
+SELECT release_year, MIN(gross)
+FROM films
+GROUP BY release_year;
+
+SELECT language, SUM(gross)
+FROM films
+GROUP BY language;
+
+SELECT country, SUM(budget)
+FROM films
+GROUP BY country;
+
+SELECT release_year, country, MAX(budget)
+FROM films
+GROUP BY release_year, country
+ORDER BY release_year, country;
+
+SELECT country, release_year, MIN(gross)
+FROM films
+GROUP BY release_year, country
+ORDER BY country, release_year;
+
+SELECT release_year
+FROM films
+GROUP BY release_year
+HAVING COUNT(title) > 200;
+
+SELECT release_year, AVG(budget) AS avg_budget, AVG(gross) AS avg_gross
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year
+HAVING AVG(budget) > 60000000
+ORDER BY avg_gross DESC;
+
+-- select country, average budget, average gross
+SELECT country, AVG(budget) AS avg_budget, AVG(gross) AS avg_gross
+-- from the films table
+FROM films
+-- group by country
+GROUP BY country
+-- where the country has more than 10 titles
+HAVING COUNT(title) > 10
+-- order by country
+ORDER BY country
+-- limit to only show 5 results
+LIMIT 5;
