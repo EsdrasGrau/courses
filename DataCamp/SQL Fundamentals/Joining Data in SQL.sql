bcaf5a0703c93d48392dc300f84cb3d285955a3c
EsdrasGrau <esdrascampos@mac.com>
2020-06-29 17:39:22 -0500

    uploading final queries from chapter 2

--- DataCamp/SQL Fundamentals/Joining Data in SQL.sql
+++ DataCamp/SQL Fundamentals/Joining Data in SQL.sql
@@ -352,3 +352,42 @@ FROM countries AS c1
     USING (code)
 -- 6. Where region like Melanesia and Micronesia
 WHERE region LIKE 'M%esia';
+
+-- 4. Select fields
+SELECT c.name AS city, l.name AS language
+-- 1. From cities (alias as c)
+FROM cities AS c
+  -- 2. Join to languages (alias as l)
+  CROSS JOIN languages AS l
+-- 3. Where c.name like Hyderabad
+WHERE c.name LIKE 'Hyder%';
+
+-- 5. Select fields
+SELECT c.name AS city, l.name AS language
+-- 1. From cities (alias as c)
+FROM cities AS c
+  -- 2. Join to languages (alias as l)
+  INNER JOIN languages AS l
+    -- 3. Match on country code
+    ON c.country_code = l.code
+-- 4. Where c.name like Hyderabad
+WHERE c.name LIKE 'Hyder%';
+
+-- Select fields
+SELECT c.name AS country, region, p.life_expectancy AS life_exp
+-- From countries (alias as c)
+FROM countries AS C
+  -- Join to populations (alias as p)
+  LEFT JOIN populations AS p
+    -- Match on country code
+    ON c.code = p.country_code
+-- Focus on 2010
+WHERE p.year = 2010
+-- Order by life_exp
+ORDER BY life_expectancy
+-- Limit to 5 records
+LIMIT 5;
+
+
+
+-- Chapter 3
