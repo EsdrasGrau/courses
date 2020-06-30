bcd32e94f1b89c34d384a4cbdd9c6a5e41e269bb
EsdrasGrau <esdrascampos@mac.com>
2020-06-29 21:22:16 -0500

    uploading queries, finished ch3, m2

--- DataCamp/SQL Fundamentals/Joining Data in SQL.sql
+++ DataCamp/SQL Fundamentals/Joining Data in SQL.sql
@@ -436,3 +436,145 @@ SELECT country_code, year
   FROM populations
 -- Order by code, year
 ORDER BY code, year;
+
+-- Select fields
+SELECT code, year
+  -- From economies
+  FROM economies
+	-- Set theory clause
+	INTERSECT
+-- Select fields
+SELECT country_code, year
+  -- From populations
+  FROM populations
+-- Order by code and year
+ORDER BY code, year;
+
+-- Select fields
+SELECT name
+  -- From countries
+  FROM countries
+	-- Set theory clause
+	INTERSECT
+-- Select fields
+SELECT name
+  -- From cities
+  FROM cities;
+
+  -- Select field
+SELECT name
+  -- From cities
+  FROM cities
+	-- Set theory clause
+	EXCEPT
+-- Select field
+SELECT capital
+  -- From countries
+  FROM countries
+-- Order by result
+ORDER BY name;
+
+-- Select field
+SELECT capital
+  -- From countries
+  FROM countries
+	-- Set theory clause
+	EXCEPT
+-- Select field
+SELECT name
+  -- From cities
+  FROM cities
+-- Order by ascending capital
+ORDER BY capital;
+
+-- Select code
+SELECT code
+  -- From countries
+  FROM COUNTRIES
+-- Where region is Middle East
+WHERE region = 'Middle East';
+
+/* SELECT code
+  FROM countries
+WHERE region = 'Middle East';
+*/
+
+-- Select field
+SELECT DISTINCT name
+  -- From languages
+  FROM languages
+-- Order by name
+ORDER BY name;
+
+-- We will use this query in Chapter 4
+
+-- Select distinct fields
+SELECT DISTINCT name
+  -- From languages
+  FROM languages
+-- Where in statement
+WHERE code IN
+  -- Subquery
+  (SELECT code
+   FROM countries
+   WHERE region = 'Middle East')
+-- Order by name
+ORDER BY name;
+
+--
+
+
+-- Select statement
+SELECT COUNT(name)
+  -- From countries
+  FROM countries
+-- Where continent is Oceania
+WHERE continent = 'Oceania';
+
+
+-- 5. Select fields (with aliases)
+SELECT c1.code,c1.name, c2.basic_unit AS currency
+  -- 1. From countries (alias as c1)
+  FROM countries AS c1
+  	-- 2. Join with currencies (alias as c2)
+  	INNER JOIN currencies AS c2
+    -- 3. Match on code
+    ON c1.code = c2.code
+-- 4. Where continent is Oceania
+WHERE c1.continent = 'Oceania';
+
+-- 3. Select fields
+SELECT code, name
+  -- 4. From Countries
+  FROM countries
+  -- 5. Where continent is Oceania
+  WHERE continent = 'Oceania'
+  	-- 1. And code not in
+  	AND code NOT IN
+  	-- 2. Subquery
+  	(SELECT code
+  	 FROM currencies);
+
+
+-- Select the city name
+SELECT name
+  -- Alias the table where city name resides
+  FROM cities AS c1
+  -- Choose only records matching the result of multiple set theory clauses
+  WHERE country_code IN
+(
+    -- Select appropriate field from economies AS e
+    SELECT e.code
+    FROM economies AS e
+    -- Get all additional (unique) values of the field from currencies AS c2
+    UNION
+    SELECT c2.code
+    FROM currencies AS c2
+    -- Exclude those appearing in populations AS p
+    EXCEPT
+    SELECT p.country_code
+    FROM populations AS p
+);
+
+
+-- Chapter 4
