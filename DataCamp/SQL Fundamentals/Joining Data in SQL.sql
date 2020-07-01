ba5031b475c17efc9e504906bb8ae4d36f2683ed
EsdrasGrau <esdrascampos@mac.com>
2020-07-01 16:34:58 -0500

    uploading queries and subqueries

--- DataCamp/SQL Fundamentals/Joining Data in SQL.sql
+++ DataCamp/SQL Fundamentals/Joining Data in SQL.sql
@@ -662,3 +662,39 @@ SELECT MAX(inflation_rate) as max_inf
       WHERE year = 2015) AS subquery
 -- Group by continent
 GROUP BY continent;
+
+-- Select fields
+SELECT name, continent, inflation_rate
+  -- From countries
+  FROM countries
+	-- Join to economies
+	INNER JOIN economies
+	-- Match on code
+	ON countries.code = economies.code
+  -- Where year is 2015
+  WHERE year = 2015
+    -- And inflation rate in subquery (alias as subquery)
+    AND inflation_rate IN (
+        SELECT MAX(inflation_rate) AS max_inf
+        FROM (
+             SELECT name, continent, inflation_rate
+             FROM countries
+             INNER JOIN economies
+             ON countries.code = economies.code
+             WHERE year = 2015) AS subquery
+      -- Group by continent
+        GROUP BY continent);
+
+
+-- Select fields
+SELECT code, inflation_rate, unemployment_rate
+  -- From economies
+  FROM economies
+  -- Where year is 2015 and code is not in
+  WHERE year = 2015 AND code NOT IN
+  	-- Subquery
+  	(SELECT code
+  	 FROM countries
+  	 WHERE (gov_form = 'Constitutional Monarchy' OR gov_form LIKE '%Republic%'))
+-- Order by inflation rate
+ORDER BY inflation_rate;
