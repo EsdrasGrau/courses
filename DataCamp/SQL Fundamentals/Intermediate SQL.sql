28b859b71b92d515572139886aba6cc352def9ff
EsdrasGrau <esdrascampos@mac.com>
2020-07-03 19:25:38 -0500

    uploading new queries

--- DataCamp/SQL Fundamentals/Intermediate SQL.sql
+++ DataCamp/SQL Fundamentals/Intermediate SQL.sql
@@ -8,6 +8,8 @@ FROM teams_germany
 -- Only include FC Schalke 04 and FC Bayern Munich
 WHERE team_long_name IN ('FC Schalke 04', 'FC Bayern Munich');
 
+
+
 -- Identify the home team as Bayern Munich, Schalke 04, or neither
 SELECT
 	CASE WHEN hometeam_id = 10189 THEN 'FC Schalke 04'
@@ -17,3 +19,134 @@ SELECT
 FROM matches_germany
 -- Group by the CASE statement alias
 GROUP BY home_team;
+
+
+
+SELECT
+	-- Select the date of the match
+	date,
+	-- Identify home wins, losses, or ties
+	CASE WHEN home_goal > away_goal THEN 'Home win!'
+         WHEN home_goal < away_goal THEN 'Home loss :('
+         ELSE 'Tie' END AS outcome
+FROM matches_spain;
+
+
+
+SELECT
+	m.date,
+	--Select the team long name column and call it 'opponent'
+	t.team_long_name AS opponent,
+	-- Complete the CASE statement with an alias
+	CASE WHEN m.home_goal > m.away_goal THEN 'Home win!'
+         WHEN m.home_goal < m.away_goal THEN 'Home loss :('
+         ELSE 'Tie' END AS outcome
+FROM matches_spain AS m
+-- Left join teams_spain onto matches_spain
+LEFT JOIN teams_spain AS t
+ON m.awayteam_id = t.team_api_id;
+
+
+
+SELECT
+	m.date,
+	t.team_long_name AS opponent,
+    -- Complete the CASE statement with an alias
+	CASE WHEN m.home_goal > m.away_goal THEN 'Barcelona win!'
+         WHEN m.home_goal < m.away_goal THEN 'Barcelona loss :('
+         ELSE 'Tie' END AS outcome
+FROM matches_spain AS m
+LEFT JOIN teams_spain AS t
+ON m.awayteam_id = t.team_api_id
+-- Filter for Barcelona as the home team
+WHERE m.hometeam_id = 8634;
+
+
+
+
+-- Select matches where Barcelona was the away team
+SELECT
+	m.date,
+	t.team_long_name AS opponent,
+	CASE WHEN m.home_goal < m.away_goal THEN 'Barcelona win!'
+         WHEN m.home_goal > m.away_goal THEN 'Barcelona loss :('
+         ELSE 'Tie' END AS outcome
+FROM matches_spain AS m
+-- Join teams_spain to matches_spain
+LEFT JOIN teams_spain AS t
+ON m.hometeam_id = t.team_api_id
+WHERE m.awayteam_id = 8634;
+
+
+
+SELECT
+	date,
+	-- Identify the home team as Barcelona or Real Madrid
+	CASE WHEN hometeam_id = 8634 THEN 'FC Barcelona'
+        ELSE 'Real Madrid CF' END AS home,
+    -- Identify the away team as Barcelona or Real Madrid
+	CASE WHEN awayteam_id = 8634 THEN 'FC Barcelona'
+        ELSE 'Real Madrid CF' END AS away
+FROM matches_spain
+WHERE (awayteam_id = 8634 OR hometeam_id = 8634)
+      AND (awayteam_id = 8633 OR hometeam_id = 8633);
+
+
+
+SELECT
+	date,
+	CASE WHEN hometeam_id = 8634 THEN 'FC Barcelona'
+         ELSE 'Real Madrid CF' END as home,
+	CASE WHEN awayteam_id = 8634 THEN 'FC Barcelona'
+         ELSE 'Real Madrid CF' END as away,
+	-- Identify all possible match outcomes
+	CASE WHEN home_goal > away_goal AND hometeam_id = 8634 THEN 'Barcelona win!'
+         WHEN home_goal > away_goal AND hometeam_id = 8633 THEN 'Real Madrid win!'
+         WHEN home_goal < away_goal AND awayteam_id = 8634 THEN 'Barcelona win!'
+         WHEN home_goal < away_goal AND awayteam_id = 8633 THEN 'Real Madrid win!'
+         ELSE 'Tie!' END AS outcome
+FROM matches_spain
+WHERE (awayteam_id = 8634 OR hometeam_id = 8634)
+      AND (awayteam_id = 8633 OR hometeam_id = 8633);
+
+
+
+-- Select team_long_name and team_api_id from team
+SELECT
+	team_long_name,
+	team_api_id
+FROM teams_italy
+-- Filter for team name
+WHERE team_long_name = 'Bologna';
+
+
+
+-- Select the season and date columns
+SELECT
+	season,
+	date,
+    -- Identify when Bologna won a match
+	CASE WHEN hometeam_id = 9857 AND home_goal > away_goal THEN 'Bologna Win'
+		 WHEN awayteam_id = 9857 AND away_goal > home_goal THEN 'Bologna Win'
+		 END AS outcome
+FROM matches_italy;
+
+
+-- To ignore all NULLs from the query
+-- Select the season, date, home_goal, and away_goal columns
+SELECT
+	season,
+    date,
+	home_goal,
+	away_goal
+FROM matches_italy
+WHERE
+-- Exclude games not won by Bologna
+	CASE WHEN hometeam_id = 9857 AND home_goal > away_goal THEN 'Bologna Win'
+		 WHEN awayteam_id = 9857 AND away_goal > home_goal THEN 'Bologna Win'
+		 END IS NOT NULL;
+
+
+
+
+break
