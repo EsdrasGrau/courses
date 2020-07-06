613f41f1256869ac2e618d472e21388c33e7438e
EsdrasGrau <esdrascampos@mac.com>
2020-07-06 16:23:30 -0500

    uploading answers

---
+++ Codewars/8 kyu/Grasshopper - Array Mean.py
@@ -0,0 +1,5 @@
+def find_average(nums):
+    if nums == []:
+        return 0
+    else:
+        return sum(nums) / len(nums)
---
+++ Codewars/8 kyu/Regex count lowercase letters.py
@@ -0,0 +1,2 @@
+def lowercase_count(strng):
+    return sum(1 for c in strng if c.islower())
