5d938d8ddf2e2805dd54532670788c3060d01582
EsdrasGrau <esdrascampos@mac.com>
2020-07-09 18:21:41 -0500

    uploading queries

--- DataCamp/SQL Fundamentals/Intermediate SQL.sql
+++ DataCamp/SQL Fundamentals/Intermediate SQL.sql
@@ -424,3 +424,39 @@ WHERE
 	s.avg_goals > (SELECT AVG(home_goal + away_goal)
                     FROM match WHERE season = '2012/2013');
 break
+
+
+-- Chapter 3
+
+SELECT
+	-- Select country ID, date, home, and away goals from match
+	main.country_id,
+    main.date,
+    main.home_goal,
+    main.away_goal
+FROM match AS main
+WHERE
+	-- Filter the main query by the subquery
+	(home_goal + away_goal) >
+        (SELECT AVG((sub.home_goal + sub.away_goal) * 3)
+         FROM match AS sub
+         -- Join the main query to the subquery in WHERE
+         WHERE main.country_id = sub.country_id);
+
+
+
+SELECT
+	-- Select country ID, date, home, and away goals from match
+	main.country_id,
+    main.date,
+    main.home_goal,
+    main.away_goal
+FROM match AS main
+WHERE
+	-- Filter for matches with the highest number of goals scored
+	(home_goal + away_goal) =
+        (SELECT MAX(sub.home_goal + sub.away_goal)
+         FROM match AS sub
+         WHERE main.country_id = sub.country_id
+               AND main.season = sub.season);
+break
