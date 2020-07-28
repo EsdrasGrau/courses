a0706f1904105b506131e3d64975eb12d0014ab1
EsdrasGrau <esdrascampos@mac.com>
2020-07-28 16:10:39 -0500

    uploading answers

---
+++ Codewars/7 kyu/GROCERY STORE- Real Price!.sql
@@ -0,0 +1,12 @@
+SELECT
+  name,
+  weight,
+  price,
+  weight,
+  CAST(
+    ROUND(CAST(price /(weight / 1000) AS NUMERIC), 2) AS DOUBLE PRECISION
+  ) AS price_per_kg
+FROM products
+ORDER BY
+  price_per_kg ASC,
+  name ASC
