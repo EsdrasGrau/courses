445cbba93334faeb50bdc37ef969b217becb5556
EsdrasGrau <esdrascampos@mac.com>
2020-07-30 20:35:38 -0500

    uploading queries

--- DataCamp/SQL Fundamentals/PostgreSQL Summary stats and Windows Functions.sql
+++ DataCamp/SQL Fundamentals/PostgreSQL Summary stats and Windows Functions.sql
@@ -6,3 +6,29 @@ SELECT
   ROW_NUMBER()OVER() AS Row_N
 FROM Summer_Medals
 ORDER BY Row_N ASC;
+
+
+
+SELECT
+  Year,
+
+  -- Assign numbers to each year
+  ROW_NUMBER() OVER() AS Row_N
+FROM (
+  SELECT DISTINCT Year
+  FROM Summer_Medals
+  ORDER BY Year ASC
+) AS Years
+ORDER BY Year ASC;
+
+
+
+SELECT
+  Year,
+  -- Assign the lowest numbers to the most recent years
+  ROW_NUMBER() OVER (ORDER BY year DESC ) AS Row_N
+FROM (
+  SELECT DISTINCT Year
+  FROM Summer_Medals
+) AS Years
+ORDER BY Year;
