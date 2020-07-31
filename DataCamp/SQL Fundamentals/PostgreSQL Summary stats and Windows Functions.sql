76a60fc095b6c2c5875c3704898e22af6080f0ef
EsdrasGrau <esdrascampos@mac.com>
2020-07-30 20:51:53 -0500

    uploading queries

--- DataCamp/SQL Fundamentals/PostgreSQL Summary stats and Windows Functions.sql
+++ DataCamp/SQL Fundamentals/PostgreSQL Summary stats and Windows Functions.sql
@@ -59,3 +59,38 @@ SELECT
   ROW_NUMBER() OVER (ORDER BY Medals DESC) AS Row_N
 FROM Athlete_Medals
 ORDER BY Medals DESC;
+
+
+
+SELECT
+  -- Return each year's champions' countries
+  year,
+  country AS champion
+FROM Summer_Medals
+WHERE
+  Discipline = 'Weightlifting' AND
+  Event = '69KG' AND
+  Gender = 'Men' AND
+  Medal = 'Gold';
+
+
+
+  WITH Weightlifting_Gold AS (
+  SELECT
+    -- Return each year's champions' countries
+    Year,
+    Country AS champion
+  FROM Summer_Medals
+  WHERE
+    Discipline = 'Weightlifting' AND
+    Event = '69KG' AND
+    Gender = 'Men' AND
+    Medal = 'Gold')
+
+SELECT
+  Year, Champion,
+  -- Fetch the previous year's champion
+  LAG (Champion) OVER
+    (ORDER BY Year ASC) AS Last_Champion
+FROM Weightlifting_Gold
+ORDER BY Year ASC;
