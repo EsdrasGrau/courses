8ed2670ee9f40b865e3242937b31a047eacfa9f6
EsdrasGrau <esdrascampos@mac.com>
2020-08-14 21:00:15 -0500

    uploading answer

---
+++ Codewars/7 kyu/SQL- Bytes in String from Ints.sql
@@ -0,0 +1,7 @@
+SELECT
+  LENGTH(number1::varchar) AS octnum1,
+  LENGTH(number2::varchar) AS octnum2,
+  LENGTH(number3::varchar) AS octnum3,
+  LENGTH(number4::varchar) AS octnum4,
+  LENGTH(number5::varchar) AS octnum5
+FROM numbers;
