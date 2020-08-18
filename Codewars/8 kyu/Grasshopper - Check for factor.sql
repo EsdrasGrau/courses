45ee263dff2f20973e2323a5d5873d21bc002655
EsdrasGrau <esdrascampos@mac.com>
2020-08-18 18:46:24 -0500

    uploading answer

--- Codewars/8 kyu/Grasshopper - Check for factor.sql
+++ Codewars/8 kyu/Grasshopper - Check for factor.sql
@@ -3,3 +3,9 @@ CASE
   WHEN base % factor = 0 THEN 'true'::boolean
   ELSE 'false'::boolean END AS res
 FROM kata;
+-- other solution
+
+select id, base % factor = 0 as res from kata
+
+--
+SELECT id, MOD(base, factor) = 0 AS res FROM kata;
