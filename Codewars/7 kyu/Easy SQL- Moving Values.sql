c75fa27ef372fb79bc3d2d07d7cd8dc5d4ade157
EsdrasGrau <esdrascampos@mac.com>
2020-08-21 15:57:26 -0500

    uploading answers

---
+++ Codewars/7 kyu/Easy SQL- Moving Values.sql
@@ -0,0 +1,3 @@
+SELECT LENGTH(name::varchar) AS id, LENGTH(legs::varchar) AS name, LENGTH(arms::varchar) AS legs,
+       LENGTH(characteristics::varchar) AS arms, LENGTH(id::varchar) AS characteristics
+FROM monsters;
