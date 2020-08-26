21d67702a11994e738b1b91accc0bd28835c0d89
EsdrasGrau <esdrascampos@mac.com>
2020-08-26 17:03:59 -0500

    uploading answer

---
+++ Codewars/8 kyu/Grasshopper - Grade book.py
@@ -0,0 +1,13 @@
+def get_grade(s1, s2, s3):
+    # Code here
+    score = (s1+s2+s3)/3
+    if score <= 100 and score >= 90:
+        return 'A'
+    elif score < 90 and score >= 80:
+        return 'B'
+    elif score < 80 and score >= 70:
+        return 'C'
+    elif score < 70 and score >= 60:
+        return 'D'
+    else:
+        return 'F'
