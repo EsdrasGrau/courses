0c4b8c98ce60bafee40ce2633ddad1d7e5b8b8ac
EsdrasGrau <esdrascampos@mac.com>
2020-09-09 18:56:29 -0500

    uploading queries

--- DataCamp/SQL for Business Analysts/Exploratory Data Analysis in SQL.sql
+++ DataCamp/SQL for Business Analysts/Exploratory Data Analysis in SQL.sql
@@ -494,4 +494,50 @@ SELECT street, COUNT(*)
  ORDER BY street;
 
 
-      
+
+
+ -- Count rows
+SELECT COUNT(*)
+  FROM evanston311
+ -- Where description includes trash or garbage
+ WHERE description ILIKE '%trash%'
+    OR description ILIKE '%garbage%';
+
+
+
+
+    -- Select categories containing Trash or Garbage
+    SELECT category
+      FROM evanston311
+     -- Use LIKE
+     WHERE category LIKE '%Trash%'
+        OR category LIKE '%Garbage%';
+
+
+
+
+-- Count rows
+SELECT COUNT(*)
+  FROM evanston311
+ -- description contains trash or garbage (any case)
+ WHERE (description ILIKE '%trash%'
+    OR description ILIKE '%garbage%')
+ -- category does not contain Trash or Garbage
+   AND category NOT LIKE '%Trash%'
+   AND category NOT LIKE '%Garbage%';
+
+
+
+   -- Count rows with each category
+   SELECT category, count(*)
+     FROM evanston311
+    WHERE (description ILIKE '%trash%'
+       OR description ILIKE '%garbage%')
+      AND category NOT LIKE '%Trash%'
+      AND category NOT LIKE '%Garbage%'
+    -- What are you counting?
+    GROUP BY category
+    --- order by most frequent values
+    ORDER BY COUNT(*) DESC
+    LIMIT 10;
+break
