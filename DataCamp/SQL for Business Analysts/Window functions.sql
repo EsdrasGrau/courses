38e80bcf823130cfd1bd0a497e477bee466f35f0
EsdrasGrau <esdrascampos@mac.com>
2021-01-29 20:44:45 -0600

    update

--- DataCamp/SQL for Business Analysts/Window functions.sql
+++ DataCamp/SQL for Business Analysts/Window functions.sql
@@ -305,3 +305,68 @@ SELECT
 FROM Thirds
 GROUP BY Third
 ORDER BY Third ASC;
+
+
+-- CHAPTER 3
+
+/*The running total (or cumulative sum) of a column helps you determine what
+each row's contribution is to the total sum.*/
+
+-- Return the athletes, the number of medals they earned, and the medals running
+-- total, ordered by the athletes' names in alphabetical order.
+WITH Athlete_Medals AS (
+  SELECT
+    Athlete, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country = 'USA' AND Medal = 'Gold'
+    AND Year >= 2000
+  GROUP BY Athlete)
+
+SELECT
+  -- Calculate the running total of athlete medals
+  Athlete,
+  Medals,
+  SUM(Medals) OVER (ORDER BY Athlete ASC) AS Max_Medals
+FROM Athlete_Medals
+ORDER BY Athlete ASC;
+
+-- Return the year, country, medals, and the maximum medals earned so far for
+-- each country, ordered by year in ascending order.
+
+WITH Country_Medals AS (
+  SELECT
+    Year, Country, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country IN ('CHN', 'KOR', 'JPN')
+    AND Medal = 'Gold' AND Year >= 2000
+  GROUP BY Year, Country)
+
+SELECT
+  -- Return the max medals earned so far per country
+  Country,
+  Year,
+  Medals,
+  MAX(Medals) OVER (PARTITION BY Country
+                ORDER BY Year ASC) AS Max_Medals
+FROM Country_Medals
+ORDER BY Country ASC, Year ASC;
+
+-- Return the year, medals earned, and minimum medals earned so far.
+
+WITH France_Medals AS (
+  SELECT
+    Year, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country = 'FRA'
+    AND Medal = 'Gold' AND Year >= 2000
+  GROUP BY Year)
+
+SELECT
+  Year,
+  Medals,
+  MIN(Medals) OVER (ORDER BY Year ASC) AS Min_Medals
+FROM France_Medals
+ORDER BY Year ASC;
