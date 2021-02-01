147f7d08367644f9c989c87d82ed2d7a42a74935
EsdrasGrau <esdrascampos@mac.com>
2021-01-31 20:32:11 -0600

    update

--- DataCamp/SQL for Business Analysts/Window functions.sql
+++ DataCamp/SQL for Business Analysts/Window functions.sql
@@ -370,3 +370,50 @@ SELECT
   MIN(Medals) OVER (ORDER BY Year ASC) AS Min_Medals
 FROM France_Medals
 ORDER BY Year ASC;
+
+-- FRAMES
+-- Return the year, medals earned, and the maximum medals earned, comparing only
+-- the current year and the next year.
+
+WITH Scandinavian_Medals AS (
+  SELECT
+    Year, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country IN ('DEN', 'NOR', 'FIN', 'SWE', 'ISL')
+    AND Medal = 'Gold'
+  GROUP BY Year)
+
+SELECT
+  -- Select each year's medals
+  Year,
+  Medals,
+  -- Get the max of the current and next years'  medals
+  MAX(Medals) OVER (ORDER BY Year ASC
+             ROWS BETWEEN CURRENT ROW
+             AND 1 FOLLOWING) AS Max_Medals
+FROM Scandinavian_Medals
+ORDER BY Year ASC;
+
+-- Return the athletes, medals earned, and the maximum medals earned, comparing
+-- only the last two and current athletes, ordering by athletes' names in alphabetical order.
+
+WITH Chinese_Medals AS (
+  SELECT
+    Athlete, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country = 'CHN' AND Medal = 'Gold'
+    AND Year >= 2000
+  GROUP BY Athlete)
+
+SELECT
+  -- Select the athletes and the medals they've earned
+  Athlete,
+  Medals,
+  -- Get the max of the last two and current rows' medals
+  MAX(Medals) OVER (ORDER BY Athlete ASC
+            ROWS BETWEEN 2 PRECEDING
+            AND CURRENT ROW) AS Max_Medals
+FROM Chinese_Medals
+ORDER BY Athlete ASC;
