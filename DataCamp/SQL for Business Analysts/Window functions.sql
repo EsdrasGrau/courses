207162df0d28b50e184cf9e2247b7820016296af
EsdrasGrau <esdrascampos@mac.com>
2021-02-01 19:54:43 -0600

    update

--- DataCamp/SQL for Business Analysts/Window functions.sql
+++ DataCamp/SQL for Business Analysts/Window functions.sql
@@ -487,3 +487,138 @@ $$) AS ct (Gender VARCHAR,
            "2012" VARCHAR)
 
 ORDER BY Gender ASC;
+
+/*Pivoting with ranking
+You want to produce an easy scannable table of the rankings of the three most
+populous EU countries by how many gold medals they've earned in the 2004 through
+2012 Olympic games. The table needs to be in this format:
+
+You'll need to count the gold medals each country has earned, produce the ranks
+of each country by medals earned, then pivot the table to this shape.
+
+1. Count the gold medals that France (FRA), the UK (GBR), and Germany (GER)
+have earned per country and year.
+*/
+-- 1st part
+-- Count the gold medals per country and year
+SELECT
+  Country,
+  Year,
+  COUNT(*) AS Awards
+FROM Summer_Medals
+WHERE
+  Country IN ('FRA', 'GBR', 'GER')
+  AND Year IN (2004, 2008, 2012)
+  AND Medal = 'Gold'
+GROUP BY Country, Year
+ORDER BY Country ASC, Year ASC
+
+-- 2nd part
+-- Select the country and year columns, then rank the three countries by how
+-- many gold medals they earned per year.
+WITH Country_Awards AS (
+  SELECT
+    Country,
+    Year,
+    COUNT(*) AS Awards
+  FROM Summer_Medals
+  WHERE
+    Country IN ('FRA', 'GBR', 'GER')
+    AND Year IN (2004, 2008, 2012)
+    AND Medal = 'Gold'
+  GROUP BY Country, Year)
+
+SELECT
+  -- Select Country and Year
+  Country,
+  Year,
+  -- Rank by gold medals earned per year
+  RANK() OVER
+      (PARTITION BY Year
+       ORDER BY Awards DESC) :: INTEGER AS rank
+FROM Country_Awards
+ORDER BY Country ASC, Year ASC;
+
+-- 3rd part
+-- Pivot the query's results by Year by filling in the new table's correct column names.
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+SELECT * FROM CROSSTAB($$
+  WITH Country_Awards AS (
+    SELECT
+      Country,
+      Year,
+      COUNT(*) AS Awards
+    FROM Summer_Medals
+    WHERE
+      Country IN ('FRA', 'GBR', 'GER')
+      AND Year IN (2004, 2008, 2012)
+      AND Medal = 'Gold'
+    GROUP BY Country, Year)
+
+  SELECT
+    Country,
+    Year,
+    RANK() OVER
+      (PARTITION BY Year
+       ORDER BY Awards DESC) :: INTEGER AS rank
+  FROM Country_Awards
+  ORDER BY Country ASC, Year ASC;
+-- Fill in the correct column names for the pivoted table
+$$) AS ct (Country VARCHAR,
+           "2004" INTEGER,
+           "2008" INTEGER,
+           "2012" INTEGER)
+
+Order by Country ASC;
+
+
+-- finish
+-- Count the gold medals awarded per country and gender.
+-- Generate Country-level gold award counts.
+-- Count the gold medals per country and gender
+SELECT
+  Country,
+  Gender,
+  COUNT(*) AS Gold_Awards
+FROM Summer_Medals
+WHERE
+  Year = 2004
+  AND Medal = 'Gold'
+  AND Country IN ('DEN', 'NOR', 'SWE')
+-- Generate Country-level subtotals
+GROUP BY Country, ROLLUP(Gender)
+ORDER BY Country ASC, Gender ASC;
+
+-- Count the medals awarded per gender and medal type.
+-- Generate all possible group-level counts (per gender and medal type
+-- subtotals and the grand total).
+
+-- Count the medals per country and medal type
+SELECT
+  Gender,
+  Medal,
+  COUNT(*) AS Awards
+FROM Summer_Medals
+WHERE
+  Year = 2012
+  AND Country = 'RUS'
+-- Get all possible group-level subtotals
+GROUP BY CUBE (Gender, Medal)
+ORDER BY Gender ASC, Medal ASC;
+
+-- Turn the nulls in the Country column to All countries, and the nulls in the
+-- Gender column to All genders.
+
+SELECT
+  -- Replace the nulls in the columns with meaningful text
+  COALESCE(Country, 'All countries') AS Country,
+  COALESCE(Gender, 'All genders') AS Gender,
+  COUNT(*) AS Awards
+FROM Summer_Medals
+WHERE
+  Year = 2004
+  AND Medal = 'Gold'
+  AND Country IN ('DEN', 'NOR', 'SWE')
+GROUP BY ROLLUP(Country, Gender)
+ORDER BY Country ASC, Gender ASC;
