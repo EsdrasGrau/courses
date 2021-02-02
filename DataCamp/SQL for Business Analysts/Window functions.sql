aa8f0b4fae6de9703358c98999be0168252ac4d9
EsdrasGrau <esdrascampos@mac.com>
2021-02-01 19:02:15 -0600

    update

--- DataCamp/SQL for Business Analysts/Window functions.sql
+++ DataCamp/SQL for Business Analysts/Window functions.sql
@@ -466,3 +466,24 @@ FROM Country_Medals
 ORDER BY Country ASC, Year ASC;
 
 -- CHAPTER 4
+-- Create the correct extension.
+-- Fill in the column names of the pivoted table.
+
+-- Create the correct extention to enable CROSSTAB
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+SELECT * FROM CROSSTAB($$
+  SELECT
+    Gender, Year, Country
+  FROM Summer_Medals
+  WHERE
+    Year IN (2008, 2012)
+    AND Medal = 'Gold'
+    AND Event = 'Pole Vault'
+  ORDER By Gender ASC, Year ASC;
+-- Fill in the correct column names for the pivoted table
+$$) AS ct (Gender VARCHAR,
+           "2008" VARCHAR,
+           "2012" VARCHAR)
+
+ORDER BY Gender ASC;
