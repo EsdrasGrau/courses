7744f7bc480ac4a2e141de7a231f1975a8e614cc
EsdrasGrau <esdrascampos@mac.com>
2021-02-09 17:23:31 -0600

    update

--- DataCamp/SQL Fundamentals/Functions for manipulating data in Postgresql.sql
+++ DataCamp/SQL Fundamentals/Functions for manipulating data in Postgresql.sql
@@ -128,3 +128,64 @@ FROM film AS f
     INNER JOIN inventory AS i ON f.film_id = i.film_id
     INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
 ORDER BY f.title;
+
+-- 1 OF 2
+-- Get the day of the week from the rental_date column.
+SELECT
+  -- Extract day of week from rental_date
+  EXTRACT(DOW FROM rental_date) AS dayofweek
+FROM rental
+LIMIT 100;
+
+-- 2 OF 2
+-- Count the total number of rentals by day of the week.
+-- Extract day of week from rental_date
+SELECT
+  EXTRACT(dow FROM rental_date) AS dayofweek,
+  -- Count the number of rentals
+  Count(rental_id) as rentals
+FROM rental
+GROUP BY 1;
+
+-- 1 of 2
+-- Extract the day of the week from the rental_date column using the alias dayofweek.
+-- Use an INTERVAL in the WHERE clause to select records for the 90 day period starting on 5/1/2005
+SELECT
+  -- Extract the day of week date part from the rental_date
+  EXTRACT(DOW FROM rental_date) AS dayofweek,
+  AGE(return_date, rental_date) AS rental_days
+FROM rental AS r
+WHERE
+  -- Use an INTERVAL for the upper bound of the rental_date
+  rental_date BETWEEN CAST('2005-05-01' AS timestamp)
+   AND CAST('2005-05-01' AS timestamp) + INTERVAL '90 day';
+
+-- 2 of 2
+-- Finally, use a CASE statement and DATE_TRUNC() to create a new column called
+-- past_due which will be TRUE if the rental_days is greater than the rental_duration
+-- therwise, it will be FALSE.
+SELECT
+  c.first_name || ' ' || c.last_name AS customer_name,
+  f.title,
+  r.rental_date,
+  -- Extract the day of week date part from the rental_date
+  EXTRACT(dow FROM r.rental_date) AS dayofweek,
+  AGE(r.return_date, r.rental_date) AS rental_days,
+  -- Use DATE_TRUNC to get days from the AGE function
+  CASE WHEN DATE_TRUNC('day', AGE(r.return_date, r.rental_date)) >
+  -- Calculate number of d
+    f.rental_duration * INTERVAL '1' day
+  THEN TRUE
+  ELSE FALSE END AS past_due
+FROM
+  film AS f
+  INNER JOIN inventory AS i
+  	ON f.film_id = i.film_id
+  INNER JOIN rental AS r
+  	ON i.inventory_id = r.inventory_id
+  INNER JOIN customer AS c
+  	ON c.customer_id = r.customer_id
+WHERE
+  -- Use an INTERVAL for the upper bound of the rental_date
+  r.rental_date BETWEEN CAST('2005-05-01' AS DATE)
+  AND CAST('2005-05-01' AS DATE) + INTERVAL '90 day';
