e59cd2248ba22d75e990fde9555902fee0be4b5a
EsdrasGrau <esdrascampos@mac.com>
2021-02-08 20:32:41 -0600

    update

--- DataCamp/SQL Fundamentals/Functions for manipulating data in Postgresql.sql
+++ DataCamp/SQL Fundamentals/Functions for manipulating data in Postgresql.sql
@@ -25,3 +25,54 @@ SELECT
 FROM INFORMATION_SCHEMA.COLUMNS
 -- For the customer table
 WHERE table_name = 'customer';
+
+-- Select the rental date and return date from the rental table.
+-- Add an INTERVAL of 3 days to the rental_date to calculate the expected return date`.
+
+-- 1 of 3
+-- Select the title and special features from the film table and compare the results between the two columns.
+-- Select the title and special features column
+SELECT
+  title,
+  special_features
+FROM film;
+
+-- 2 of 3
+-- Select all films that have a special feature Trailers by filtering on the first index of the special_features ARRAY.
+-- Select the title and special features column
+SELECT
+  title,
+  special_features
+FROM film
+-- Use the array index of the special_features column
+WHERE special_features[1] = 'Trailers';
+
+-- 3 of 3
+-- Now let's select all films that have Deleted Scenes in the second index of the special_features ARRAY.
+-- Select the title and special features column
+SELECT
+  title,
+  special_features
+FROM film
+-- Use the array index of the special_features column
+WHERE special_features[2] = 'Deleted Scenes';
+
+-- Match 'Trailers' in any index of the special_features ARRAY regardless of position.
+SELECT
+  title,
+  special_features
+FROM film
+-- Modify the query to use the ANY function
+WHERE 'Trailers' = ANY (special_features);
+
+-- Use the contains operator to match the text Deleted Scenes in the special_features column.
+SELECT
+  title,
+  special_features
+FROM film
+-- Filter where special_features contains 'Deleted Scenes'
+WHERE special_features @> ARRAY['Deleted Scenes'];
+
+
+
+-- CHAPTER 2
