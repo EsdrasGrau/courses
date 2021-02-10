14ad269538679b77a51049cd4974c77f6811c68e
EsdrasGrau <esdrascampos@mac.com>
2021-02-10 17:13:49 -0600

    update

--- DataCamp/SQL Fundamentals/Functions for manipulating data in Postgresql.sql
+++ DataCamp/SQL Fundamentals/Functions for manipulating data in Postgresql.sql
@@ -260,3 +260,64 @@ SELECT
   -- Extract the characters to the right of the '@'
   SUBSTRING(email FROM POSITION('@' IN email)+1 FOR CHAR_LENGTH(email)) AS domain
 FROM customer;
+
+-- 1 OF 3
+-- Add a single space to the end or right of the first_name column using a padding function.
+-- Use the || operator to concatenate the padded first_name to the last_name column.
+-- Concatenate the padded first_name and last_name
+SELECT
+	RPAD(first_name, LENGTH(first_name)+1) || last_name AS full_name
+FROM customer;
+
+-- 2 OF 3
+-- Now add a single space to the left or beginning of the last_name column using a different padding function than the first step.
+-- Use the || operator to concatenate the first_name column to the padded last_name.
+-- Concatenate the first_name and last_name
+SELECT
+	first_name || LPAD(last_name, LENGTH(last_name)+1) AS full_name
+FROM customer;
+
+-- 3 OF 3
+-- Add a single space to the right or end of the first_name column.
+-- Add the characters < to the right or end of last_name column.
+-- Finally, add the characters > to the right or end of the email column.
+
+-- Concatenate the first_name and last_name
+SELECT
+	RPAD(first_name, LENGTH(first_name)+1)
+    || RPAD(last_name, LENGTH(last_name)+2, ' <')
+    || RPAD(email, LENGTH(email)+1, '>') AS full_email
+FROM customer;
+
+-- Convert the film category name to uppercase and use the CONCAT() concatenate it with the title.
+-- Truncate the description to the first 50 characters and make sure there is no leading or trailing whitespace after truncating.
+-- Concatenate the uppercase category name and film title
+SELECT
+  CONCAT(UPPER(c.name), ': ', f.title) AS film_category,
+  -- Truncate the description remove trailing whitespace
+  TRIM(LEFT(description, 50)) AS film_desc
+FROM
+  film AS f
+  INNER JOIN film_category AS fc
+  	ON f.film_id = fc.film_id
+  INNER JOIN category AS c
+  	ON fc.category_id = c.category_id;
+
+-- Get the first 50 characters of the description column
+-- Determine the position of the last whitespace character of the truncated
+-- description column and subtract it from the number 50 as the second parameter in the first function above.
+SELECT
+  UPPER(c.name) || ': ' || f.title AS film_category,
+  -- Truncate the description without cutting off a word
+  LEFT(description, 50 -
+    -- Subtract the position of the first whitespace character
+    POSITION(
+      ' ' IN REVERSE(LEFT(description, 50))
+    )
+  )
+FROM
+  film AS f
+  INNER JOIN film_category AS fc
+  	ON f.film_id = fc.film_id
+  INNER JOIN category AS c
+  	ON fc.category_id = c.category_id;
