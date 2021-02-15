ab57e8ad0f1a039f371caa077ead459cd18b0ef6
EsdrasGrau <esdrascampos@mac.com>
2021-02-15 11:55:51 -0600

    update

--- DataCamp/SQL for Business Analysts/Data driven decision making.sql
+++ DataCamp/SQL for Business Analysts/Data driven decision making.sql
@@ -1,1 +1,19 @@
 -- CHAPTER 1
+-- CHAPTER 2
+SELECT country, -- For each country report the earliest date when an account was created
+	MIN(date_account_start) AS first_account
+FROM customers
+GROUP BY country
+ORDER BY first_account ASC;
+
+
+SELECT customer_id, -- Report the customer_id
+      AVG(rating),  -- Report the average rating per customer
+      COUNT(rating),  -- Report the number of ratings per customer
+      COUNT(*)  -- Report the number of movie rentals per customer
+FROM renting
+GROUP BY customer_id
+HAVING COUNT(*) > 7 -- Select only customers with more than 7 movie rentals
+ORDER BY 2 ASC; -- Order by the average rating in ascending order
+
+
