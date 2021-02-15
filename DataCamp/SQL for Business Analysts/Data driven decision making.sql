a3d58340ae6e0506e883a91b3ae1f488e1a15044
EsdrasGrau <esdrascampos@mac.com>
2021-02-15 13:43:06 -0600

    update

--- DataCamp/SQL for Business Analysts/Data driven decision making.sql
+++ DataCamp/SQL for Business Analysts/Data driven decision making.sql
@@ -27,3 +27,79 @@ ON r.movie_id = m.movie_id
 -- Only look at movie rentals in 2018
 WHERE date_renting BETWEEN '2018-01-01' AND '2018-12-31' ;
 
+-- 1 of 3
+-- Use a join to get the movie title and price for each movie rental
+SELECT m.title, -- Use a join to get the movie title and price for each movie rental
+       m.renting_price
+FROM renting AS r
+LEFT JOIN movies AS m
+ON r.movie_id = m.movie_id;
+
+-- 2 of 2
+-- Report the total income for each movie.
+-- Order the result by decreasing income.
+SELECT rm.title, -- Report the income from movie rentals for each movie 
+       SUM(rm.renting_price) AS income_movie
+FROM
+       (SELECT m.title,  
+               m.renting_price
+       FROM renting AS r
+       LEFT JOIN movies AS m
+       ON r.movie_id=m.movie_id) AS rm
+GROUP BY rm.title
+ORDER BY income_movie DESC; -- Order the result by decreasing income
+
+-- 2 of 3
+-- No 3rd step
+
+SELECT a.gender, -- Report for male and female actors from the USA 
+       MAX(a.year_of_birth), -- The year of birth of the oldest actor
+       MIN(a.year_of_birth) -- The year of birth of the youngest actor
+FROM
+   (SELECT *-- Use a subsequen SELECT to get all information about actors from the USA
+   FROM actors
+   WHERE nationality = 'USA') AS a -- Give the table the name a
+GROUP BY a.gender;
+
+SELECT m.title, 
+COUNT(*),
+AVG(r.rating)
+FROM renting AS r
+LEFT JOIN customers AS c
+ON c.customer_id = r.customer_id
+LEFT JOIN movies AS m
+ON m.movie_id = r.movie_id
+WHERE c.date_of_birth BETWEEN '1970-01-01' AND '1979-12-31'
+GROUP BY m.title
+HAVING COUNT(*) > 1 -- Remove movies with only one rental
+ORDER BY AVG(r.rating) DESC; -- Order with highest rating first
+
+SELECT a.name,  c.gender,
+       COUNT(*) AS number_views, 
+       AVG(r.rating) AS avg_rating
+FROM renting as r
+LEFT JOIN customers AS c
+ON r.customer_id = c.customer_id
+LEFT JOIN actsin as ai
+ON r.movie_id = ai.movie_id
+LEFT JOIN actors as a
+ON ai.actor_id = a.actor_id
+WHERE c.country = 'Spain' -- Select only customers from Spain
+GROUP BY a.name, c.gender
+HAVING AVG(r.rating) IS NOT NULL 
+  AND COUNT(*) > 5 
+ORDER BY avg_rating DESC, number_views DESC;
+
+SELECT 
+	c.country,                    -- For each country report
+	COUNT(*) AS number_renting, -- The number of movie rentals
+	AVG(r.rating) AS average_rating, -- The average rating
+	SUM(m.renting_price) AS revenue         -- The revenue from movie rentals
+FROM renting AS r
+LEFT JOIN customers AS c
+ON c.customer_id = r.customer_id
+LEFT JOIN movies AS m
+ON m.movie_id = r.movie_id
+WHERE date_renting >= '2019-01-01'
+GROUP BY 1;
+
