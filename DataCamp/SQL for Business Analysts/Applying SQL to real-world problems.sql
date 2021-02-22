71d5165fd61bb36905baae1db563e5428e9f2de7
EsdrasGrau <esdrascampos@mac.com>
2021-02-22 14:27:31 -0600

    update

--- DataCamp/SQL for Business Analysts/Applying SQL to real-world problems.sql
+++ DataCamp/SQL for Business Analysts/Applying SQL to real-world problems.sql
@@ -24,3 +24,42 @@ INNER JOIN language AS l
 WHERE release_year = 2010
   AND rating = 'G'
 GROUP BY name;
+
+-- CHAPTER 3
+-- Increase rental_rate by one dollar for R-rated movies
+UPDATE film
+SET rental_rate = rental_rate + 1
+WHERE rating = 'R'
+
+UPDATE film
+SET rental_rate = rental_rate - 1
+WHERE film_id IN
+  (SELECT film_id from actor AS a
+   INNER JOIN film_actor AS f
+      ON a.actor_id = f.actor_id
+   WHERE last_name IN ('WILLIS', 'CHASE', 'WINSLET', 'GUINESS', 'HUDSON'));
+
+
+-- Delete films that cost most than 25 dollars
+DELETE FROM film
+WHERE replacement_cost > 25;
+
+-- Use the list of film_id values to DELETE all R & NC-17 rated films from inventory.
+DELETE FROM inventory
+WHERE film_id IN (
+  SELECT film_id FROM film
+  WHERE rating IN ('R', 'NC-17')
+);
+
+-- Delete records from the `film` table that are either rated as R or NC-17.
+DELETE FROM film
+WHERE rating IN ('R', 'NC-17')
+
+SELECT category AS film_category, 
+       AVG(length) AS average_length
+FROM film AS f
+INNER JOIN category AS c
+  ON f.film_id = c.film_id
+WHERE release_year BETWEEN 2005
+  AND 2010
+GROUP BY category;
