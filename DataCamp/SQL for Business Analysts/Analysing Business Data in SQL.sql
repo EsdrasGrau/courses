5159ec3d717966f0a79a69acfc790d0c7f242859
EsdrasGrau <esdrascampos@mac.com>
2021-03-01 16:15:49 -0600

    update

--- DataCamp/SQL for Business Analysts/Analysing Business Data in SQL.sql
+++ DataCamp/SQL for Business Analysts/Analysing Business Data in SQL.sql
@@ -255,3 +255,116 @@ FROM mau
 -- Order by month in ascending order
 ORDER BY delivr_month ASC;
 
+
+/* Fetch the previous month's MAU in the mau_with_lag CTE..
+Select the month and the delta between its MAU and the previous month's MAU.
+Order by month in ascending order. */
+WITH mau AS (
+  SELECT
+    DATE_TRUNC('month', order_date) :: DATE AS delivr_month,
+    COUNT(DISTINCT user_id) AS mau
+  FROM orders
+  GROUP BY delivr_month),
+
+  mau_with_lag AS (
+  SELECT
+    delivr_month,
+    mau,
+    -- Fetch the previous month's MAU
+    COALESCE(
+      LAG(mau) OVER (ORDER BY delivr_month ASC),
+    0) AS last_mau
+  FROM mau)
+
+SELECT
+  -- Calculate each month's delta of MAUs
+  delivr_month,
+  mau - last_mau AS mau_delta
+FROM mau_with_lag
+-- Order by month in ascending order
+ORDER BY delivr_month ASC;
+
+/* Select the month and its MoM MAU growth rate.
+Order by month in ascending order.
+*/
+WITH mau AS (
+  SELECT
+    DATE_TRUNC('month', order_date) :: DATE AS delivr_month,
+    COUNT(DISTINCT user_id) AS mau
+  FROM orders
+  GROUP BY delivr_month),
+
+  mau_with_lag AS (
+  SELECT
+    delivr_month,
+    mau,
+    GREATEST(
+      LAG(mau) OVER (ORDER BY delivr_month ASC),
+    1) AS last_mau
+  FROM mau)
+
+SELECT
+  -- Calculate the MoM MAU growth rates
+  delivr_month,
+  ROUND(
+    (mau - last_mau):: numeric / last_mau,
+  2) AS growth
+FROM mau_with_lag
+-- Order by month in ascending order
+ORDER BY delivr_month ASC;
+
+/*Count the unique orders per month.
+Fetch each month's previous and current orders.
+Return a table of MoM order growth rates.*/
+WITH orders AS (
+  SELECT
+    DATE_TRUNC('month', order_date) :: DATE AS delivr_month,
+    --  Count the unique order IDs
+    COUNT(DISTINCT order_id) AS orders
+  FROM orders
+  GROUP BY delivr_month),
+
+  orders_with_lag AS (
+  SELECT
+    delivr_month,
+    -- Fetch each month's current and previous orders
+    orders,
+    COALESCE(
+      LAG(orders) OVER (ORDER BY delivr_month ASC),
+    1) AS last_orders
+  FROM orders)
+
+SELECT
+  delivr_month,
+  -- Calculate the MoM order growth rate
+  ROUND(
+    (orders - last_orders) :: NUMERIC / last_orders,
+  2) AS growth
+FROM orders_with_lag
+ORDER BY delivr_month ASC;
+
+WITH user_monthly_activity AS (
+  SELECT DISTINCT
+    DATE_TRUNC('month', order_date) :: DATE AS delivr_month,
+    user_id
+  FROM orders)
+
+SELECT
+  -- Calculate the MoM retention rates
+  previous.delivr_month,
+  ROUND(
+    COUNT(DISTINCT current.user_id) :: NUMERIC /
+    GREATEST(COUNT(DISTINCT previous.user_id),1),
+  2) AS retention_rate
+FROM user_monthly_activity AS previous
+LEFT JOIN user_monthly_activity AS current
+-- Fill in the user and month join conditions
+ON previous.user_id = current.user_id
+AND previous.delivr_month = (current.delivr_month - INTERVAL '1 month')
+GROUP BY previous.delivr_month
+ORDER BY previous.delivr_month ASC;
+
+
+
+-- 3RD CHAPTER
+
